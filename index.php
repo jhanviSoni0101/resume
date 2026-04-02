01af1514647691a3720bbd4aaf4c0767da04d0e5
jahnvi soni <jhanviSoni0101@gmail.com>
2026-04-02 12:15:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include('./components/headers.php') ?>
+</head>
+
+<body>
+    <?php include('./components/header.php') ?>
+    <div class="container main">
+        <h1>Welcome🌸</h1>
+        <p>Something new is coming...🤭</p>
+    </div>
+</body>
+
+</html>
